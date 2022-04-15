093d373e4938704172149afcc0a6ed8707599ed6
Eihab Ahmed <eihabahmed@gmail.com>
2022-04-15 23:35:05 +0200

    Updated Project Structure

--- app/src/main/java/com/bbk/studentsmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/bbk/studentsmvvm/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bbk.studentsmvvm
+package com.bbk.studentsmvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bbk.studentsmvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
