71654408de0ee0915c0041e371ea71ba87605ca3
Eihab Ahmed <eihabahmed@gmail.com>
2022-04-16 20:55:38 +0200

    Adjusted text view binding

--- app/src/main/java/com/bbk/studentsmvvm/bindingadapters/StudentRowBinding.kt
+++ app/src/main/java/com/bbk/studentsmvvm/bindingadapters/StudentRowBinding.kt
@@ -1,6 +1,7 @@
 package com.bbk.studentsmvvm.bindingadapters
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import coil.load
 import com.bbk.studentsmvvm.R
@@ -8,12 +9,26 @@ import com.bbk.studentsmvvm.R
 class StudentRowBinding {
 
     companion object {
+
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
         fun loadImageFromUrl(imageView: ImageView, imageUrl: String?) {
             imageView.load(imageUrl) {
                 crossfade(600)
                 error(R.drawable.ic_placeholder)
+            }
+        }
+
+        @BindingAdapter("showText")
+        @JvmStatic
+        fun showText(textView: TextView, value: Int) {
+            when (textView.id) {
+                R.id.age_textView -> {
+                    textView.text = "$value years old"
+                }
+                R.id.grade_textView -> {
+                    textView.text = "Grade $value"
+                }
             }
         }
     }
